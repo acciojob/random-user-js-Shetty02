b083430719d86708dae4b30fd3260bc9b7311e06
Nehal Shetty <98724754+Shetty02@users.noreply.github.com>
2023-01-28 18:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let user = {}
 const imgElement = document.getElementById("img"),
  nameElement = document.getElementById("name"),
  additionalInfo = document.getElementById("add-info"),
- infoBtn = Array.from(document.querySelectorAll("[data-id]"))
+ infoBtn = Array.from(document.querySelectorAll("[id]"))
  fetchingUser = document.getElementById("getUser");
 
 const renderUsersBasicDetails = (user) =>{
